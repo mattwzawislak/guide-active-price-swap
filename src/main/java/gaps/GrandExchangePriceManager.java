ecb0246fd4ffb6c91b2f9edb9923b6d5ca5dec4e
Matt Zawislak <mattwzawislak@gmail.com>
2025-03-24 09:14:53 -0600

    Update UI when changing price

--- src/main/java/gaps/GrandExchangePriceManager.java
+++ src/main/java/gaps/GrandExchangePriceManager.java
@@ -4,7 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.ScriptEvent;
 import net.runelite.api.VarPlayer;
+import net.runelite.api.events.VarbitChanged;
 import net.runelite.api.widgets.ComponentID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.game.ItemManager;
 
 import javax.inject.Inject;
@@ -15,7 +18,7 @@ import javax.inject.Singleton;
 public class GrandExchangePriceManager
 {
 	private static final int VARBIT_GRAND_EXCHANGE_OFFER_PRICE = 4398;
-	private static final int GRAND_EXCHANGE_CHANGE_PRICE_SCRIPT_ID = 778;
+	private static final int GRAND_EXCHANGE_REFRESH_UI_SCRIPT_ID = 779;
 	private static final int GRAND_EXCHANGE_OFFER_CONFIRM = 30474269;
 
 	@Inject
@@ -26,15 +29,15 @@ public class GrandExchangePriceManager
 
 	public boolean isOfferSetToActivePrice()
 	{
-		int activePrice = getActivePrice();
-		int offerPrice = getOfferPrice();
+		final int activePrice = getActivePrice();
+		final int offerPrice = getOfferPrice();
 		return offerPrice == activePrice;
 	}
 
 	public void setActivePrice()
 	{
-		int activePrice = getActivePrice();
-		int offerPrice = getOfferPrice();
+		final int activePrice = getActivePrice();
+		final int offerPrice = getOfferPrice();
 		if (activePrice == -1)
 		{
 			log.trace("Asked to set active price but offer was not set");
@@ -47,30 +50,30 @@ public class GrandExchangePriceManager
 			return;
 		}
 
-		// TODO: this doesn't work?
-		final int delta = activePrice - offerPrice;
-		client.runScript(
-				GRAND_EXCHANGE_CHANGE_PRICE_SCRIPT_ID,
+		setOfferPrice(activePrice);
+
+		final Widget grandExchangeContainer = client.getWidget(ComponentID.GRAND_EXCHANGE_OFFER_CONTAINER);
+		final ScriptEvent scriptEvent = client.createScriptEvent(GRAND_EXCHANGE_REFRESH_UI_SCRIPT_ID,
 				0,
-				delta,
-				ScriptEvent.MENU_OP,
 				ComponentID.GRAND_EXCHANGE_OFFER_CONTAINER,
-				0,
-				6,
-				50,
-				20,
-				21,
-				22,
-				23,
-				27,
-				34,
-				41,
-				45,
-				46,
+				0, // Item icon container
+				6, // +1k
+				50, // +1k
+				20, // "Buy offer" / "Sell offer"
+				21, // Buy icon / Sell icon
+				22, // Item icon container
+				23, // Item icon
+				27, // Item name
+				34, // Quantity number picker
+				41, // Price number picker
+				45, // Total price
+				46, // Search button
 				GRAND_EXCHANGE_OFFER_CONFIRM,
 				58,
-				7
-		);
+				7);
+		scriptEvent.setSource(grandExchangeContainer);
+		scriptEvent.setOp(1);
+		scriptEvent.run();
 	}
 
 	private int getOfferItemId()
@@ -83,9 +86,14 @@ public class GrandExchangePriceManager
 		return client.getVarbitValue(VARBIT_GRAND_EXCHANGE_OFFER_PRICE);
 	}
 
+	private void setOfferPrice(int offerPrice)
+	{
+		client.setVarbit(VARBIT_GRAND_EXCHANGE_OFFER_PRICE, offerPrice);
+	}
+
 	private int getActivePrice()
 	{
-		int itemId = getOfferItemId();
+		final int itemId = getOfferItemId();
 		if (itemId == -1)
 		{
 			return -1;
